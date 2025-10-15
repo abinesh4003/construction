2e895013fe90df102378d1cdf2b9254277446fb6
abinesh4003 <abineshsmart322@gmail.com>
2025-10-15 15:12:59 +0530

    checking failed

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: false,
-  images: {
-    domains: [], // add your allowed image domains here
-  },
-  async redirects() {
-    return [
-      // Non-www HTTP/HTTPS -> www HTTPS
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'vargheseconstruction.vercel.app' }],
-        destination: 'https://www.vargheseconstruction.vercel.app/:path*',
-        permanent: true,
-      },
-      {
-        source: '/',
-        has: [{ type: 'host', value: 'vargheseconstruction.vercel.app' }],
-        destination: 'https://www.vargheseconstruction.vercel.app/',
-        permanent: true,
-      },
+    images: {
+        domains: [
+        ],
 
-      // Optional: ensure root www HTTP -> HTTPS
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.vargheseconstruction.vercel.app' }],
-        destination: 'https://www.vargheseconstruction.vercel.app/:path*',
-        permanent: true,
-      },
-    ];
-  },
+    }
 };
 
 export default nextConfig;
