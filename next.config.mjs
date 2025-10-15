32664767590e332610c2e95392192fc799358b67
abinesh4003 <abineshsmart322@gmail.com>
2025-10-15 14:02:22 +0530

    updated redircts

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    trailingSlash: false,
     images: {
         domains: [
         ],
 
-    }
+    },
+      async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'vargheseconstruction.com' }],
+        destination: 'https://www.vargheseconstruction.com/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.vargheseconstruction.com' }],
+        missing: [{ type: 'host', value: 'https://www.vargheseconstruction.com' }],
+        destination: 'https://www.vargheseconstruction.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
