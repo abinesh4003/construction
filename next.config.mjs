d7707f80fe310446ad4e8099ff4e7a1d505bc05d
abinesh4003 <abineshsmart322@gmail.com>
2025-10-17 14:03:49 +0530

    redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: [
-        ],
-    async redirects() {
+  trailingSlash: false,
+  images: {
+    domains: [],
+  },
+  async redirects() {
     return [
       {
         source: '/:path*',
         has: [
-          { type: 'host', value: 'vargheseconstruction.vervel.app' } // apex domain
+          { type: 'host', value: 'vargheseconstruction.vercel.app' } // apex domain
         ],
         destination: 'https://vargheseconstruction.vercel.app/:path*', // redirect to www
         permanent: true, // 301 redirect
       },
     ];
   },
-    }
 };
 
 export default nextConfig;
-
-// async redirects() {
-//     return [
-//       {
-//         source: '/:path*',
-//         has: [
-//           { type: 'host', value: 'vargheseconstruction.com' } // apex domain
-//         ],
-//         destination: 'https://www.vargheseconstruction.com/:path*', // redirect to www
-//         permanent: true, // 301 redirect
-//       },
-//     ];
-//   },
